84de1cbc2abbd6f23ca0176f117608a3e8fbf060
YeLwin <yelwin@adddress.com>
2020-01-12 18:09:31 +0630

    Customer CRUD Created!

--- app/Controllers/Http/CustomerController.js
+++ app/Controllers/Http/CustomerController.js
@@ -1,92 +1,83 @@
 'use strict'
+const Customer=use('App/Models/Customer')
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with customers
- */
 class CustomerController {
-  /**
-   * Show a list of all customers.
-   * GET customers
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+ 
+  async index ({ request, response }) {
+    const customers=await Customer.all()
+    response.status(200).json({
+      greeting:'Here are your customers.',
+      data:customers
+    })
   }
 
-  /**
-   * Render a form to be used for creating a new customer.
-   * GET customers/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async store ({ request, response ,params:{id}}) {
+    const { name, description } =request.post()
+    
+    debugger
+    const customer=new Customer();
+    customer.name=name
+    customer.description=description
+    await customer.save()
+    /*const customer=await Customer.create({name,description}) */
+    response.status(201).json({
+      message:'Successfully created a new customer',
+      data:customer
+    })
   }
 
-  /**
-   * Create/save a new customer.
-   * POST customers
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async show ({ params:{id}, response}) {
+    const customer= await Customer.find(id);
+    if(customer){
+      response.status(200).json({
+        message:'Here are your customer',
+        data:customer
+      })
+    }else{
+      response.status(404).json({
+        greeting:'Customer Not Found',
+        id
+      })
+    }
   }
 
-  /**
-   * Display a single customer.
-   * GET customers/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+
+  async update ({ params:{id}, request, response }) {
+    const customer=await Customer.find(id)
+    if(customer){
+      const {name , description}= request.post()
+
+      customer.name=name
+      customer.description=description
+
+      await customer.save()
+      response.status(200).json({
+        message:'Successfully updated a new customer',
+        data:customer
+      })
+    }else{
+      response.status(404).json({
+        message:'Customer not found',
+        id
+      })
+    }
+    
   }
 
-  /**
-   * Render a form to update an existing customer.
-   * GET customers/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update customer details.
-   * PUT or PATCH customers/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a customer with id.
-   * DELETE customers/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params:{id}, request, response }) {
+    const customer=await Customer.find(id)
+    if(customer){
+      await customer.delete()
+      response.status(200).json({
+        message:'Successfully updated a new customer',
+       id
+      })
+    }else{
+      response.status(404).json({
+        message:'Customer not found',
+        id
+      })
+    }
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -1,17 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
@@ -19,3 +7,9 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.get('/customers','CustomerController.index')
+Route.post('/customers','CustomerController.store')
+Route.get('/customers/:id','CustomerController.show')
+Route.patch('/customers/:id','CustomerController.update')
+Route.delete('/customers/:id','CustomerController.destroy')
